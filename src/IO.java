b2be64c66e077effdecb0b1bdadafaff6a7b0669
Ana Paula Simplicio <ana.simplicio@zup.com.br>
2021-07-23 20:41:40 -0300

    Classes sistema,io

---
+++ src/IO.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IO {
+
+    public static void mostratexto(String texto){
+        System.out.println(texto);
+    }
+    public static Scanner criarvalor(){
+        return new Scanner(System.in);
+
+
+
+
+    }
+}
+
---
+++ src/sistema.java
@@ -0,0 +1,13 @@
+public class sistema {
+
+    public static void cadastroDeImovel(){
+        IO.mostratexto( "digitar o endereco" );
+        String endereco = IO.criarvalor().next();
+        IO.mostratexto("digitar o valor do aluguel");
+        String aluguel = IO.criarvalor().next();
+        IO.mostratexto(" Funcionario");
+        String funcionario = IO.criarvalor().next();
+
+
+    }
+}
