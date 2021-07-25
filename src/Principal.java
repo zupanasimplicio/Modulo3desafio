741f0065596a960827aca285b799b27a25847049
Ana Paula Simplicio <ana.simplicio@zup.com.br>
2021-07-25 17:02:14 -0300

    classe principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Principal {
+public class Principal {
+
+    public static void main(String[] args) {
+
+        try {
+            Sistema.executarSistema();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
