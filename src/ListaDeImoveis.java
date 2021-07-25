d1de99eda904d98bb782ca27f56ca91fb25a601f
Ana Paula Simplicio <ana.simplicio@zup.com.br>
2021-07-25 16:56:34 -0300

    Classe lista de moradores

--- src/ListaDeImoveis.java
+++ src/ListaDeImoveis.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ListaDeImoveis {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaDeImoveis {
+
+    private static List<Imovel>imoveis =new ArrayList<>();
+
+    public static void mostrarListaDeImoveis(){
+        System.out.println(" Lista de imoveis ");
+        for (Imovel imovel: imoveis) {
+            imovel.exibirValoresDosImoveis();
+        }
+    }
+    public static void adicionarImovel(Imovel imovel){
+        imoveis.add(imovel);
+    }
+
 }
